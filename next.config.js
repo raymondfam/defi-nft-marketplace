24a0d26f6a441e726b6235c90ae8474d74683119
raymondfam <raymondfam@gmail.com>
2022-08-09 21:57:54 -0500

    change basepath again

--- next.config.js
+++ next.config.js
@@ -5,6 +5,8 @@ const nextConfig = {
         loader: "akamai",
         path: "",
     },
+    basePath: "/defi-nft-marketplace",
+    assetPrefix: "/defi-nft-marketplace",
 }
 
 module.exports = nextConfig

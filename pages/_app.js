d3e0c3b202ee5efa449e305712634c2b17fdcd83
raymondfam <raymondfam@gmail.com>
2022-08-10 01:34:33 -0500

    env again

--- pages/_app.js
+++ pages/_app.js
@@ -17,7 +17,7 @@ function MyApp({ Component, pageProps }) {
             <Head>
                 <title>NFT Marketplace</title>
                 <meta name="description" content="NFT Marketplace" />
-                <link rel="icon" href="defi-nft-marketplace/favicon.ico" />
+                <link rel="icon" href="/favicon.ico" />
             </Head>
             <MoralisProvider initializeOnMount={false}>
                 <ApolloProvider client={client}>

b8ac0f70d307b7a57f8faeabcd14417186a3078e
raymondfam <raymondfam@gmail.com>
2022-08-11 05:53:13 -0500

    test material ui button

--- components/Header.js
+++ components/Header.js
@@ -1,25 +1,28 @@
 import { ConnectButton } from "web3uikit"
 import Link from "next/link"
+import CssBaseline from "@mui/material/CssBaseline"
 
 export default function Header() {
     return (
-        <nav className="p-5 border-b-2 flex flex-row justify-between items-center">
-            <h1 className="py-4 px-4 font-bold text-3xl">NFT Marketplace</h1>
-            <div className="flex flex-row items-center">
-                <Link href="/">
-                    <a className="mr-4 p-6">Home</a>
-                </Link>
-                <Link href="/mint">
-                    <a className="mr-4 p-6">Mint</a>
-                </Link>
-                <Link href="/sell-nft">
-                    <a className="mr-4 p-6">Sell NFT</a>
-                </Link>
-                <Link href="/nft">
-                    <a className="mr-4 p-6">Marketplace</a>
-                </Link>
-                <ConnectButton moralisAuth={false} />
-            </div>
-        </nav>
+        <CssBaseline>
+            <nav className="p-5 border-b-2 flex flex-row justify-between items-center">
+                <h1 className="py-4 px-4 font-bold text-3xl">NFT Marketplace</h1>
+                <div className="flex flex-row items-center">
+                    <Link href="/">
+                        <a className="mr-4 p-6 no-underline text-black">Home</a>
+                    </Link>
+                    <Link href="/mint">
+                        <a className="mr-4 p-6 no-underline text-black">Mint</a>
+                    </Link>
+                    <Link href="/sell-nft">
+                        <a className="mr-4 p-6 no-underline text-black">Sell NFT</a>
+                    </Link>
+                    <Link href="/nft">
+                        <a className="mr-4 p-6 no-underline text-black">Marketplace</a>
+                    </Link>
+                    <ConnectButton moralisAuth={false} />
+                </div>
+            </nav>
+        </CssBaseline>
     )
 }
--- components/mutual-components/MutualsClaimForm.js
+++ components/mutual-components/MutualsClaimForm.js
@@ -1,12 +1,15 @@
 import { useWeb3Contract, useMoralis } from "react-moralis"
 import { wethAbi, wethAddress, mutualsAbi, mutualsAddress } from "../../constants"
-import { Button, useNotification, Form } from "web3uikit"
+// import { useNotification } from "web3uikit"
+import { Button } from "@mui/material"
 import { ethers } from "ethers"
 import { useState, useEffect } from "react"
+import CssBaseline from "@mui/material/CssBaseline"
+
 export default function JumpForm() {
     const { address, account, isWeb3Enabled } = useMoralis()
     const { runContractFunction } = useWeb3Contract()
-    const dispatch = useNotification()
+    // const dispatch = useNotification()
 
     let rewardsOptions = {
         abi: mutualsAbi,
@@ -23,27 +26,27 @@ export default function JumpForm() {
         },
     })
 
-    function displayNotification(title, type) {
-        dispatch({
-            type: type,
-            title: title,
-            position: "topR",
-        })
-    }
+    // function displayNotification(title, type) {
+    //     dispatch({
+    //         type: type,
+    //         title: title,
+    //         position: "topR",
+    //     })
+    // }
 
     let [earned, setEarned] = useState("")
     async function handleSubmit() {
-        const tx = await runContractFunction({
-            params: rewardsOptions,
-            onError: (error) => {
-                console.log(error)
-                displayNotification(error.message, "error")
-                return error
-            },
-            onSuccess: () => {
-                displayNotification(`Successfully claimed ${earned}!`, "success")
-            },
-        })
+        // const tx = await runContractFunction({
+        //     params: rewardsOptions,
+        //     onError: (error) => {
+        //         console.log(error)
+        //         displayNotification(error.message, "error")
+        //         return error
+        //     },
+        //     onSuccess: () => {
+        //         displayNotification(`Successfully claimed ${earned}!`, "success")
+        //     },
+        // })
     }
     const setRewardsEarned = async () => {
         let balance = await getEarningsBalance()
@@ -55,18 +58,21 @@ export default function JumpForm() {
         }
     }, [account, isWeb3Enabled])
     return (
-        <div className="shadow-xl rounded-xl p-8 px-12">
-            <h3 className="font-bold text-2xl mb-4 text-slate-500">Claim Rewards</h3>
-            <Button
-                text="Claim all rewards"
-                theme="primary"
-                style={{ padding: "8px", margin: "12px 0px" }}
-                onClick={handleSubmit}
-                disabled={!Number(earned) ? true : false}
-            ></Button>
-            <p className=" text-lg">
-                Rewards Earned: <span className="font-semibold">{earned}</span>
-            </p>
-        </div>
+        <CssBaseline>
+            <div className="shadow-xl rounded-xl p-8 px-12">
+                <h3 className="font-bold text-2xl mb-4 text-slate-500">Claim Rewards</h3>
+                <Button
+                    className="p-2 my-3"
+                    variant="contained"
+                    onClick={handleSubmit}
+                    disabled={!Number(earned) ? true : false}
+                >
+                    Claim rewards
+                </Button>
+                <p className=" text-lg">
+                    Rewards Earned: <span className="font-semibold">{earned}</span>
+                </p>
+            </div>
+        </CssBaseline>
     )
 }
